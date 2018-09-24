96e77998e96f74e4d929aa2b0020ba2fa162aed5
d.zaviryukha <d.zaviryukha@astoundcommerce.com>
2018-09-25 02:29:06 +0300

    0205-JSX

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import ReactDOM from 'react-dom';
 const title = React.createElement('h1', null, 'React Todo');
 const subtitle = React.createElement('p', { className: 'subtitle' }, 'This is my first app on React');
 const container = React.createElement('div', null, title, subtitle);
+const app = (
+    <div>
+        <h1>React ToDo</h1>
+        <p className="subtitle">This is my first app on React</p>
+    </div>
+);
 
-ReactDOM.render(container, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
