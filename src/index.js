64ccb0825c78f75bf6212ac18e74e3af8684a874
d.zaviryukha <d.zaviryukha@astoundcommerce.com>
2018-09-25 14:09:03 +0300

    0301-install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-    return (
-        <div>
-            <h1>React ToDo</h1>
-            <p className="subtitle">This is my first app on React</p>
-        </div>
-    );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
