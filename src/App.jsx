99bf47db2ab8c5be42be8d819e68c1f409fa4e22
d.zaviryukha <d.zaviryukha@astoundcommerce.com>
2018-09-25 16:59:37 +0300

    0305-composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from './components/Header';
+import Todo from './components/Todo';
+
 function App(props) {
     console.log(props);
     return (
         <main>
-            <header>
-                <h1>{props.title}</h1>
-            </header>
+            <Header title={props.title} />
             <section className="todo-list">
-                <div className="todo completed">
-                    <button className="checkbox icon">
-                        <i className="material-icons">check_box</i>
-                    </button>
-
-                    <span className="todo-title">Learn JavaScript</span>
-
-                    <button className="delete icon">
-                        <i className="material-icons">delete</i>
-                    </button>
-                </div>
-                <div className="todo">
-                    <button className="checkbox icon">
-                        <i className="material-icons">check_box_outline_blank</i>
-                    </button>
-
-                    <span className="todo-title">Learn React</span>
-
-                    <button className="delete icon">
-                        <i className="material-icons">delete</i>
-                    </button>
-                </div>
+                <Todo title={"Learn JavaScript"} completed={true} />
+                <Todo title={"Learn React"} completed={false} />
             </section>
         </main>
     );
