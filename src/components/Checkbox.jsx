3d8476ae60a2e8855f1cfa5b33e6ef9c3b860449
d.zaviryukha <d.zaviryukha@astoundcommerce.com>
2018-09-26 08:16:07 +0300

    0403-interaction-of-component

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Checkbox(props) {
     return (
-        <button className="checkbox icon">
+        <button className="checkbox icon" onClick={props.onChange}>
             <i className="material-icons">
                 {props.checked ? 'check_box' : 'check_box_outline_blank'}
             </i>
@@ -12,7 +12,8 @@ function Checkbox(props) {
 }
 
 Checkbox.propTypes = {
-    Checked: PropTypes.bool.isRequired
+    Checked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
 }
 
 export default Checkbox;
