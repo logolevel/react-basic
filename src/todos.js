f9cb06e002b3299b9f9246ac31e0445114e10ad0
logolevel <logolevel@gmail.com>
2019-01-20 10:44:34 +0200

    0602-THEORY-life-cycle-component

--- src/todos.js
+++ src/todos.js
@@ -4,16 +4,16 @@ const todos = [
         title: 'Lean JavaScript',
         completed: true
     },
-    {
-        id: 2,
-        title: 'Lean React',
-        completed: false
-    },
-    {
-        id: 3,
-        title: 'Write Application',
-        completed: false
-    }
+    // {
+    //     id: 2,
+    //     title: 'Lean React',
+    //     completed: false
+    // },
+    // {
+    //     id: 3,
+    //     title: 'Write Application',
+    //     completed: false
+    // }
 ];
 
 export default todos;
